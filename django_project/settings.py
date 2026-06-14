c32fc5d5ac85869ba72454854da69654981e367c
Aulia Adni <aulia.adni.012@gmail.com>
2026-06-14 18:25:55 +0700

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "accounts",
     "pages",
+    "articles",
 ]
 AUTH_USER_MODEL = "accounts.CustomUser"
 
@@ -109,7 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Jakarta"
+
 
 USE_I18N = True
 
